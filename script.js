ff6d12f6844a14b3d0b64fa17051b8adef59a68e
codessbabe <oana.anghelachi@gmail.com>
2020-05-07 12:39:11 +0200

    Save data to localStorage

--- script.js
+++ script.js
@@ -8,10 +8,21 @@ const movieSelect = document.getElementById('movie');
 let ticketPrice = +movieSelect.value;
 console.log(typeof ticketPrice);
 
+
 // Functions
-// Update Selected Total and Count 
+// Save selected movie index and price 
+function setMovieData(movieIndex, moviePrice) { 
+  localStorage.setItem('selectedMovieIndex', movieIndex );
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+// Update Total and Count 
 function updateSelectedCount() { 
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  // Copy selected seats into an array 
+  const seatsIndex = [...selectedSeats].map( (seat) => [...seats].indexOf(seat) );
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+  
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = '$' + selectedSeatsCount * ticketPrice; 
@@ -22,17 +33,14 @@ function updateSelectedCount() {
 // Movie select event 
 movieSelect.addEventListener('change', e => { 
   ticketPrice = +e.target.value // add the binary operator '+' to coerce the string to a number
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 })
-
 
 // Seat click event
 container.addEventListener('click', (e) => { 
   if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {  
-    console.log(e.target); 
     e.target.classList.toggle('selected'); 
-
     updateSelectedCount();
-
   }
 })
